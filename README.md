0bf313bff910e2fbe03d9789f2c666e1cce97502
22DP1DOnuf <127093801+daniilsonufrijuks@users.noreply.github.com>
2024-06-16 11:02:42 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PHP-PORTFOLIO
+# MY PORTFOLIO
+
+Easy and good looking portfolio template for you!
+
+![alt text](image.png)
+
+Source: https://github.com/saileshrijal/portfolio-website-template
+
---
+++ inc/mainmenue.php
@@ -0,0 +1,15 @@
+<div id="global_menu_button_container">
+    <p class="site_title">Daniils Onufrijuks</p>
+    <i class="fa fa-bars" id="global_menu_button"></i>
+</div>
+
+<ul id="global_menu_items">
+    <div>
+        <i class="fa fa-close" id="global_menu_close_button"></i>
+        <li><a href="index.html"><i class="fa fa-home"></i> About</a></li>
+        <li><a href="resume.html"><i class="fas fa-file-alt"></i> Resume</a></li>
+        <!-- <li><a href="blogs.html"><i class="fas fa-pen-nib"></i> Blogs</a></li> -->
+        <li><a href="services.html"><i class="fas fa-cog"></i> Services</a></li>
+        <li><a href="contact.html"><i class="fas fa-phone-square"></i> Contact</a></li>
+    </div>
+</ul>
